de2bf903e357fb8efb8e2fbbff271ba9675247b1
jisen <289736032@qq.com>
2018-03-15 15:53:57 +0800

    第一次提交

---
+++ ReactDemo/app/component/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Home extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render(){
+        return(
+            <div>
+                这是首页
+            </div>
+        );
+    }
+}
+export default Home;
---
+++ ReactDemo/entryBuild/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from '../app/component/Home.jsx';
+
+ReactDOM.render(<Home/>,document.getElementById('testComponent'));
